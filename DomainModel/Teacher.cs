2ca37418c507e077570e45dc77e48d08740358fb
Nab <17692533+Nab69@users.noreply.github.com>
2022-10-19 12:30:06 +0200

    model

---
+++ DomainModel/Teacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel
+{
+    public class Teacher : Person
+    {
+        [Required]
+        public string Discipline { get; set; }
+
+        [Range(1000, 3000)]
+        public int Salary { get; set; }
+    }
+}
